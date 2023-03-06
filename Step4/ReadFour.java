0f36e2a4a20a84112e0d32b4bd52001f485ee378
Nina <nina.emiko.mcnair@gmail.com>
2023-03-06 17:27:58 -0500

    Completed step 4

--- Step4/ReadFour.java
+++ Step4/ReadFour.java
@@ -6,7 +6,9 @@ import java.io.IOException;
 
 public class ReadFour
 {
-    /**
+    /** print sum of all numbers in input.txt file
+     *
+     *
      * Yes, you can look at the other 
      * examples in this lab to build your solution here.
      */
@@ -14,5 +16,30 @@ public class ReadFour
         {
             // Print out a running total of all the
             // values in the input file.
+
+            try {
+                Scanner fileIn = new Scanner(new File("input.txt"));
+                //int sum = 0;
+                int sum = 0;
+                while (fileIn.hasNext())
+                {
+                    // Reads the entire line as a string
+                    String lineIn = fileIn.nextLine();
+                    System.out.println(lineIn);
+                    sum += Integer.parseInt(lineIn);
+                    // Split the line into a String array
+//                    String[] input = lineIn.split(",");
+//                    int sum = 0;
+//                    for (int i = 0; i < input.length; i ++) {
+//                        sum += Integer.parseInt(input[i]);
+//                    }
+//                    System.out.println(sum);
+
+                }
+                System.out.println(sum);
+            }
+            catch (IOException e) {
+                System.out.println("File not found");
+            }
         }
 }
